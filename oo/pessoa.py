5d6206b44f334ee9c19377e26b24e91ab04a0a7b
pnfab <pnfabio@gmail.com>
2022-03-11 15:36:22 -0300

    Criado atributo de olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2 #atributos de Classe (default)
+
     def __init__(self, *filhos, nome=None, idade=40):
         self.idade = idade
         self.nome = nome
@@ -19,6 +21,12 @@ if __name__ == '__main__':
         print(filho.nome)
     fran.sobrenome = 'Nascimento'
     del fran.filhos
+    fran.olhos = 1
+    del fran.olhos
+    Pessoa.olhos = 3
     print(fran.__dict__)
     print(heitor.__dict__)
+    print(Pessoa.olhos)
+    print(fran.olhos)
+    print(id(fran.olhos), id(heitor.olhos), id(Pessoa.olhos))
 
